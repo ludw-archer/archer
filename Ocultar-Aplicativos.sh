8dc95a1b8a0ce343b2449744752f6923ad3c26c4
ludw-archer <seizdova@aol.com>
2025-11-05 19:17:26 -0300

    Tune Files
    A collection of the files to configure the Arch Linux after installation from scratch. Each file is a important part of the system in the "/etc" directory and complain the important settings for tune the System.

---
+++ Ocultar-Aplicativos.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.local/share/applications
+
+backup_dir=~/.local/desktop_backups/applications_backup_$(date +%Y%m%d_%H%M%S)
+mkdir -p "$backup_dir"
+
+apps_to_move=(
+    "qv4l2.desktop"
+    "qvidcap.desktop"
+    "avahi-discover.desktop"
+    "bssh.desktop"
+    "bvnc.desktop"
+)
+
+for app in "${apps_to_move[@]}"; do
+    src="/usr/share/applications/$app"
+    dest_local="$HOME/.local/share/applications/$app"
+    dest_backup="$backup_dir/$app"
+
+    if [ -f "$src" ]; then
+        cp "$src" "$dest_backup"
+        cp "$src" "$dest_local"
+        sed -i 's/^NoDisplay=.*/NoDisplay=true/; t; $aNoDisplay=true' "$dest_local"
+        sudo rm -f "$src"
+    else
+        echo "Arquivo $app não encontrado em /usr/share/applications, ignorando."
+    fi
+done
+
+update-desktop-database ~/.local/share/applications/
+
+echo "Todos os arquivos foram movidos, ocultados e backup criado em $backup_dir com sucesso!"
+
